d3ef35092d49b746e0773f9f44d22e395314e6c6
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-07-18 20:31:32 -0500

    jobs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    server: 'https://personal-portfolio-server.irvinfiz.now.sh/graph',
+    jobs: []
+
+  }
+
+  async componentDidMount(){
+    const query = `query{
+      jobs {
+        _id
+        name
+        link
+        type
+        logo
+      }
+    }`
+
+    const url = this.state.server;
+    const opts = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query })
+    };
+    const res = await fetch(url, opts);
+    const data = await res.json();
+    console.log(data.data.jobs)
+    this.setState({jobs: data.data.jobs })
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
