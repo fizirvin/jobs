8b35c93280666c61c83abbb8c7b84e171846fdcc
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-07-26 03:46:15 -0500

    jobs sort

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Jobs from './components/jobs'
 
@@ -37,7 +36,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <Jobs></Jobs>
+        <Jobs jobs={this.state.jobs}></Jobs>
       </div>
     );
   }
--- src/components/jobs.js
+++ src/components/jobs.js
@@ -1,8 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
+import '../styles/jobs.css'
 
-const Jobs = () =>{
+const Jobs = ({jobs}) =>{
 
-    return(<div>Hello</div>);
+    const renderJobs = jobs.sort( (a,b) => {
+            if (a.type === 'platform') return -1;
+            
+            return 0;
+        })
+        .map( ({_id, name, link, type, logo}, index) => 
+        <tr key={_id}>
+            <td className='table_row'>{index+1}</td>
+            <td className='table_row'>{logo}</td>
+            <td className='table_row'>{name}</td>
+            <td className='table_row'><a href={link}>{link}</a></td>
+            <td className='table_row'>{type}</td>
+        </tr>)
+  
+    return(
+        <div>
+            <table className='jobs_table'>
+                <thead>
+                    <tr>
+                        <th className='table_row'>#</th>
+                        <th className='table_row'>Logo</th>
+                        <th className='table_row'>Site</th>
+                        <th className='table_row'>Link</th>
+                        <th className='table_row'>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderJobs}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Jobs;
